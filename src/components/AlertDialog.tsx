7b6699e0852a07b1be25da101182088e4e9858c0
sjoon627 <sjoon627@naver.com>
2025-07-23 09:24:17 +0900

    feat: Dialog 컴포넌트 추가 및 AlertDialog에서 경로 수정

--- src/components/AlertDialog.tsx
+++ src/components/AlertDialog.tsx
@@ -8,7 +8,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/Dialog"
 
 type AlertDialogVariant = "default" | "destructive" | "warning" | "success"
 
