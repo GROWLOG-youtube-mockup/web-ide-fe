8d99cf1538f59633bbbd7316dbd1eb6813552d3b
lamarck009 <simula1009@gmail.com>
2025-07-29 19:10:27 +0900

    feat: 탭 관련 기능 추가 - 모두 닫기 - 오른쪽 닫기 - 다른 탭 닫기

--- src/stores/editor-file-store.ts
+++ src/stores/editor-file-store.ts
@@ -9,11 +9,28 @@ interface FileTabStore {
   activeTabId: string | null
   openFile: (filePath: string) => void
   closeTab: (filePath: string) => void
+  closeAllTabs: () => void
   switchTab: (filePath: string) => void
+  closeOtherTabs: (keepFilePath: string) => void
+  closeTabsToTheRight: (fromFilePath: string) => void
 }
 
 export const useFileTabStore = create<FileTabStore>(set => ({
   activeTabId: null,
+
+  closeAllTabs: () =>
+    set(state => ({
+      ...state,
+      activeTabId: null,
+      openTabs: [],
+    })),
+
+  closeOtherTabs: (keepFilePath: string) =>
+    set(state => ({
+      ...state,
+      activeTabId: keepFilePath, // 남은 탭이 활성 탭
+      openTabs: state.openTabs.filter(tab => tab.filePath === keepFilePath),
+    })),
 
   closeTab: filePath =>
     set(state => {
@@ -26,6 +43,21 @@ export const useFileTabStore = create<FileTabStore>(set => ({
             ? tabsAfterClosing[tabsAfterClosing.length - 1]?.filePath || null
             : state.activeTabId,
         openTabs: tabsAfterClosing,
+      }
+    }),
+
+  closeTabsToTheRight: (fromFilePath: string) =>
+    set(state => {
+      const currentIndex = state.openTabs.findIndex(tab => tab.filePath === fromFilePath)
+      const tabsToKeep = state.openTabs.slice(0, currentIndex + 1) // 현재 탭 포함해서 왼쪽만
+
+      return {
+        ...state,
+        // 활성 탭이 닫힌 탭 중에 있다면 현재 탭으로 변경
+        activeTabId: tabsToKeep.some(tab => tab.filePath === state.activeTabId)
+          ? state.activeTabId
+          : fromFilePath,
+        openTabs: tabsToKeep,
       }
     }),
 
