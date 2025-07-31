3747121ca9e09014060e29309f6b5a8f5921a056
lamarck009 <simula1009@gmail.com>
2025-07-31 20:16:33 +0900

    feat: 참조 파일 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
+import { QueryClientProvider } from "@tanstack/react-query"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { ToastProvider } from "@/components/common/ToastContext"
+import { queryClient } from "@/lib/query-client"
 import { router } from "@/stores/router"
 import "./index.css"
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <ToastProvider>
-      <RouterProvider router={router} />
-    </ToastProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
